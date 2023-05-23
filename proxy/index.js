b8f39dc67b6ebfdf6649dc73d93ed2b50d11f70e
Kenneth <palencia.kenneth69@gmail.com>
2023-05-24 02:42:45 +0800

    routing

--- proxy/index.js
+++ proxy/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
+//const cors = require("cors");
 const {createProxyMiddleware} = require("http-proxy-middleware");
 
-app.use(cors());
-app.get("/", createProxyMiddleware({target: 'https://office-of-the-extension-services.000webhostapp.com/apiConnection', changeOrigin:true}))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+app.get("/", 
+        createProxyMiddleware({target: 'https://office-of-the-extension-services.000webhostapp.com/apiConnection', 
+        changeOrigin:true}),
+        (req, res) => {
+            console.log(res);
+        }
+    )
 
 app.listen(4200,()=>{
     console.log("proxy started");
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
     // dbName: 'id20554492_oeswebsite_db',
     // dbUser: 'id20554492_root',
     // dbPassword: '000WEBhost@keanna'
-    apiUrl: 'http://localhost:8080/apiConnection'  //proxy url in development
+    apiUrl: 'https://office-of-the-extension-services.000webhostapp.com/apiConnection'  //proxy url in development
 };
