c4ed40b4fe9b7592e0438791f1650553d419723e
Kenneth <palencia.kenneth69@gmail.com>
2023-05-30 21:34:15 +0800

    ui designing

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  registerForm!:FormGroup;
+  hide:boolean=true;
+  email:any;
+  password:any;
+  cpassword:any;
+
+  constructor(
+    public _fb:FormBuilder,
+    private _api:ApiService,
+    private router:Router
+  ) { }
+
+  ngOnInit(): void {
+
+    this.registerForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      cpassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onRegister(form: any){
+
+    const credentials = {
+      email: form.controls['email'].value,
+      password: form.controls['password'].value,
+      cpassword: form.controls['cpassword'].value
+    }
+
+    this._api.toRegister(credentials).subscribe((response: any) => {
+      console.log(response);
+    
+    
+    })
+
+
+  }
+
+}
